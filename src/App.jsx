314676d400fb2b85779ffcd2db04fdfb5c27fc71
Bano-Barry <barrymamadoubano14@gmail.com>
2025-12-31 23:04:24 +0000

    init squelette jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-function App() {
- 
-  return (
+import loader from "./assets/loader.svg";
 
-      <main>
-      
-      </main>
-  
+function App() {
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loading icon" />
+      </div>
+      <p className="city-name">Conakry</p>
+      <p className="country-name">Guinea</p>
+      <p className="temperature">17°C</p>
+      <div className="info-icon-container">
+        <img src="/icons/01d.svg" alt="weather icon" />
+      </div>
+    </main>
   );
 }
 
